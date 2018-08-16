97889042cad600c26c64e837cd81e2877c2c131c
huntzhan <huntzhan.dev@gmail.com>
2018-08-16 06:23:40 +0000

    Bump version: 0.5.1 → 0.5.2

--- setup.py
+++ setup.py
@@ -47,6 +47,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/huntzhan/cnt.rulebase',
-    version='0.5.1',
+    version='0.5.2',
     zip_safe=False,
 )
