714853d2152b6b5fcd0959540a4bdc838b6303a5
Hunt Zhan <huntzhan.dev@gmail.com>
2018-10-21 02:21:55 +0800

    Add __all__ to __init__.py to enable autodoc.

--- cnt/rulebase/__init__.py
+++ cnt/rulebase/__init__.py
@@ -5,6 +5,15 @@ __author__ = """Hunt Zhan"""
 __email__ = 'huntzhan.dev@gmail.com'
 __version__ = '0.6.4'
 
+__all__ = [
+        'sentseg',
+        'dlmseg',
+        'replace_chinese_chars',
+        'replace_english_chars',
+        'replace_digits',
+        'replace_delimiters',
+]
+
 from cnt.rulebase.sentence_segmenter import sentseg
 from cnt.rulebase.delimiter_segmenter import dlmseg
 from cnt.rulebase.char_replacer import (
